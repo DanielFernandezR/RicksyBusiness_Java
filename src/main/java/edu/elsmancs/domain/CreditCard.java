e06f816051fb33a385f57533eb0f8b60aca6e80d
DanielFernandezR <dfernandez@cifpfbmoll.eu>
2020-02-24 21:19:50 +0100

    feat(CreditCard): Crear método pay y caso test

--- src/main/java/edu/elsmancs/domain/CreditCard.java
+++ src/main/java/edu/elsmancs/domain/CreditCard.java
@@ -12,15 +12,24 @@ public class CreditCard {
 		this.number = number;
 	}
 
-	String number() {
+	public String number() {
 		return this.number;
 	}
 
-	double credit() {
+	public double credit() {
 		return this.credit;
 	}
 
 	public String toString() {
 		return "owner: " + this.owner + "\n" + "number: " + number() + "\n" + "credit: " + credit() + this.Symbol;
+	}
+
+	public boolean pay(double initialCredits) {
+		if (credit() - initialCredits >= 0) {
+			this.credit -= initialCredits;
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
--- src/test/java/edu/elsmancs/domain/CreditCardTest.java
+++ src/test/java/edu/elsmancs/domain/CreditCardTest.java
@@ -1,5 +1,6 @@
 package edu.elsmancs.domain;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -13,5 +14,13 @@ public class CreditCardTest {
 		CreditCard prueba = new CreditCard("Dani Fernandez", "4876490012345678");
 		assertTrue(prueba.toString().contains("Dani"));
 		assertTrue(prueba.toString().contains("4876"));
+	}
+
+	@Test
+	public void payTest() {
+		CreditCard prueba = new CreditCard("Dani Fernandez", "4876490012345678");
+		double delta = 0.001;
+		prueba.pay(200);
+		assertEquals(2800, prueba.credit(), delta);
 	}
 }
