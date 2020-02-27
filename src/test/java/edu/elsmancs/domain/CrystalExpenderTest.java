ee358e857b69d568f496db1a24997251d84dcd58
DanielFernandezR <dfernandez@cifpfbmoll.eu>
2020-02-27 12:16:09 +0100

    test(CrystalExpender): Añadir todos los casos posibles en casos test

--- src/test/java/edu/elsmancs/domain/CrystalExpenderTest.java
+++ src/test/java/edu/elsmancs/domain/CrystalExpenderTest.java
@@ -1,26 +1,37 @@
 package edu.elsmancs.domain;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class CrystalExpenderTest {
 
-	CrystalExpender prueba;
+	CrystalExpender expender = null;
 
 	@Test
 	public void stockTest() {
-		CrystalExpender prueba = new CrystalExpender(5, 40);
-		assertTrue(prueba.toString().contains("stock: 5"));
+		CrystalExpender expender = new CrystalExpender(5, 40);
+		assertEquals(5, expender.stock());
 	}
 
 	@Test
 	public void dispatchTest() {
-		CrystalExpender prueba = new CrystalExpender(5, 40);
+		CrystalExpender expender = new CrystalExpender(5, 40);
 		CreditCard invitado = new CreditCard("Dani Fernandez", "4876490012345678");
 		double delta = 0.001;
-		prueba.dispatch(invitado);
+		expender.dispatch(invitado);
 		assertEquals(2960, invitado.credit(), delta);
+		assertEquals(4, expender.stock());
+	}
+
+	@Test
+	public void dispatchTestNoCreditos() {
+		CrystalExpender expender = new CrystalExpender(5, 40);
+		CreditCard invitado = new CreditCard("Dani Fernandez", "4876490012345678");
+		double delta = 0.001;
+		invitado.pay(2990);
+		expender.dispatch(invitado);
+		assertEquals(10, invitado.credit(), delta);
+		assertEquals(5, expender.stock());
 	}
 }
